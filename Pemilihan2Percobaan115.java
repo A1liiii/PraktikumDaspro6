cfecc5406a747e7296a59aefb0f1f999f52be4a3
A1liiii <mhmmdalimirza@gmail.com>
2024-10-03 19:43:54 +0700

    menambahkan program tahun kabisat pada percobaan2

---
+++ Pemilihan2Percobaan115.java
@@ -0,0 +1,20 @@
+package meet6;
+import java.util.Scanner;
+public class Pemilihan2Percobaan115 {
+    public static void main(String[] args) {
+        Scanner input15 = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.println("Masukkan Tahun : ");
+        tahun = input15.nextInt();
+
+        if (tahun % 4 == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("tahun kabisat.");
+            }
+        } else {
+            System.out.println("bukanlah tahun kabisat.");
+        }
+    }
+}
