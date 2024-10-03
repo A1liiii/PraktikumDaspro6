27323ac6d28261886865d8028c2b1d2bc2211d0a
A1liiii <mhmmdalimirza@gmail.com>
2024-10-04 02:36:09 +0700

    berhasil memodifikasi percobaan1

--- Pemilihan2Percobaan115.java
+++ Pemilihan2Percobaan115.java
@@ -12,6 +12,8 @@ public class Pemilihan2Percobaan115 {
         if (tahun % 4 == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("tahun kabisat.");
+            } else {
+                System.out.println("bukan tahun kabisat");
             }
         } else {
             System.out.println("bukanlah tahun kabisat.");
